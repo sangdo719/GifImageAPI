9bfde27fe7af5b5267825d7f5e3b26d8fce69589
developersage <sangdo719@gmail.com>
2022-05-16 15:28:57 -0700

    restore

--- app/src/main/java/com/example/anniegif/repo/GifRepo.kt
+++ app/src/main/java/com/example/anniegif/repo/GifRepo.kt
@@ -5,7 +5,27 @@ import com.example.anniegif.repo.remote.RetrofitInstance
 import retrofit2.Response
 
 object GifRepo {
+<<<<<<< Updated upstream
     suspend fun getGifAPI(path: String, amount: Int): Response<GifModel> {
         return RetrofitInstance.gifService.getPath(path, amount)
+=======
+
+    private val gifService by lazy { RetrofitInstance.gifService }
+
+    suspend fun getCategories(): List<Category> = MoshiAdapter.run {
+        val response = gifService.getCategory()
+        val jsonStr = response.string().run { substring(1, length.minus(2)) }
+            .split("},")
+            .map{
+                val split = it.split(":{")
+                val jsonObject = String.format("{ %s, %s }", "\"title\": ${split[0]}", split[1])
+                jsonObject
+            }
+        fromJson(jsonStr.toString())
+    }
+
+    suspend fun getGif(path: String, amount: Int): Response<GifModel> {
+        return gifService.getGif(path, amount)
+>>>>>>> Stashed changes
     }
 }
